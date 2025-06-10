6d4e1f58dcf78f290ed374e783aea3cea1237e3e
laura salesse <salesse.laura@gmail.com>
2025-06-10 17:29:40 +0200

    trying to freeze the top of the page but not done yet

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,21 +2,21 @@ class ProductsController < ApplicationController
 
   # function for the page showing the list of products
   def index
-
     if params[:query].present?
       sql_subquery = <<~SQL
         products.name ILIKE :query
         OR products.category ILIKE :query
         OR products.description ILIKE :query
+        OR products.status ILIKE :query
         OR CAST(products.current_price AS TEXT) ILIKE :query
         OR CAST(products.last_month_volume AS TEXT) ILIKE :query
         OR CAST(products.contract_end_date AS TEXT) ILIKE :query
-        OR "suppliers"."company_name" ILIKE :query
+        OR users.company_name ILIKE :query
       SQL
-        @products = Product.joins(:supplier).where(sql_subquery, query: "%#{params[:query]}%")
+
+      @products = Product.joins(:supplier).where(sql_subquery, query: "%#{params[:query]}%")
     else
       @products = Product.all
     end
   end
-
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,6 @@
 class Product < ApplicationRecord
-  belongs_to :supplier, class_name: 'User'
+  belongs_to :supplier, class_name: 'User' #this means “Treat supplier_id as a foreign key to the users table — and treat the related record as a User.”
+
   has_many :renegotiations
 
   validates :name, presence: true
