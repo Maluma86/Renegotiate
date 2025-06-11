b92d764e20903c376e607c41a5dffe9ec43703c7
Maluma Tolley maluma_0014@yahoo.com <maluma_0014@yahoo.com>
2025-06-11 13:33:43 +0100

    still testing

--- app/models/renegotiation.rb
+++ app/models/renegotiation.rb
@@ -1,17 +1,3 @@
-<<<<<<< HEAD
-# class Renegotiation < ApplicationRecord
-#   has_one :product
-=======
-class Renegotiation < ApplicationRecord
-  belongs_to :product
-  belongs_to :buyer, class_name: 'User'
-  belongs_to :supplier, class_name: 'User'
->>>>>>> 45ec7ffed3dadd3fad9c7d5a0b8b64435146f424
-
-#   validates :tone, presence: true
-#   validates :min_target, presence: true
-#   validates :max_target, presence: true
-# end
 class Renegotiation < ApplicationRecord
   belongs_to :product
   belongs_to :buyer,    class_name: "User"
