84ebadf150d75da1e4116258855c4b50334563ae
Maluma Tolley maluma_0014@yahoo.com <maluma_0014@yahoo.com>
2025-06-18 15:46:51 +0100

    Precompile all Stimulus controllers in assets pipeline

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,14 +1,16 @@
+
 Rails.application.config.assets.version = "1.0"
 
-# Tell Sprockets where to find your Stimulus controllers
+# Tell Sprockets where to find your JS/controllers
 Rails.application.config.assets.paths << Rails.root.join("app/javascript")
 Rails.application.config.assets.paths << Rails.root.join("app/javascript/controllers")
 
-# Precompile your controllers so they show up under /assets/controllers/*.js
+# Precompile *all* your Stimulus controllers
 Rails.application.config.assets.precompile += %w[
-  controllers/renegotiations_controller.js
-  controllers/renegotiation_show_controller.js
+  controllers/*.js
 ]
 
-# Any other assets you already precompile…
-Rails.application.config.assets.precompile += %w( bootstrap.min.js popper.js template_upload.csv )
+# Any other assets you were already precompiling
+Rails.application.config.assets.precompile += %w[
+  bootstrap.min.js popper.js template_upload.csv
+]
