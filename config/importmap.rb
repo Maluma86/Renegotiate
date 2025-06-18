667fad750308ec02d6e8ad089404f026ed03fce5
Maluma Tolley maluma_0014@yahoo.com <maluma_0014@yahoo.com>
2025-06-18 15:08:12 +0100

    fixed stimulus

--- config/importmap.rb
+++ config/importmap.rb
@@ -2,9 +2,16 @@ pin "application", preload: true
 pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus",      to: "stimulus.min.js"
 
-# Pin your entrypoint
+# Load all of your Stimulus controllers from app/javascript/controllers
 pin_all_from "app/javascript/controllers", under: "controllers"
-pin "controllers/renegotiations_controller", to: "controllers/renegotiations_controller.js"
+
+# Explicitly pin the two controllers you wrote
+pin "controllers/renegotiations_controller",    to: "controllers/renegotiations_controller.js"
 pin "controllers/renegotiation_show_controller", to: "controllers/renegotiation_show_controller.js"
 
+# Third-party libraries
+pin "bootstrap",           to: "bootstrap.min.js", preload: true
+pin "@popperjs/core",      to: "popper.js",          preload: true
+
+# Your main entrypoint
 pin "application", to: "application.js"
