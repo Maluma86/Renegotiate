6ad4feb7efccc643f28d1645c49711bcbcefb76b
Maluma Tolley maluma_0014@yahoo.com <maluma_0014@yahoo.com>
2025-06-18 16:01:12 +0100

    testing 7

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,13 +1,13 @@
-// app/javascript/application.js
+
 import "@hotwired/turbo-rails"
 import { Application } from "@hotwired/stimulus"
 
-import RenegotiationsController     from "./controllers/renegotiations_controller"
-import RenegotiationShowController  from "./controllers/renegotiation_show_controller"
+import RenegotiationsController     from "controllers/renegotiations_controller"
+import RenegotiationShowController  from "controllers/renegotiation_show_controller"
 
 // 1) Start Stimulus
 const application = Application.start()
 
-// 2) Register your controllers by name:
+// 2) Register your controllers
 application.register("renegotiations",     RenegotiationsController)
 application.register("renegotiation-show",  RenegotiationShowController)
