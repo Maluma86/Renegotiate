8f8c3b8f437b86848a5bf2b2b50f43200f0c4f57
Hubertus Wasmer <hwasmer1@gmail.com>
2025-06-19 18:20:26 +0200

    Sync schema.rb from origin/master to allow clean merge

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_06_16_124455) do
+ActiveRecord::Schema[7.1].define(version: 2025_06_19_103804) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -78,6 +78,14 @@ ActiveRecord::Schema[7.1].define(version: 2025_06_16_124455) do
     t.index ["buyer_id"], name: "index_renegotiations_on_buyer_id"
     t.index ["product_id"], name: "index_renegotiations_on_product_id"
     t.index ["supplier_id"], name: "index_renegotiations_on_supplier_id"
+  end
+
+  create_table "solid_cable_messages", force: :cascade do |t|
+    t.text "channel"
+    t.text "payload"
+    t.datetime "created_at", null: false
+    t.index ["channel"], name: "index_solid_cable_messages_on_channel"
+    t.index ["created_at"], name: "index_solid_cable_messages_on_created_at"
   end
 
   create_table "solid_queue_blocked_executions", force: :cascade do |t|
