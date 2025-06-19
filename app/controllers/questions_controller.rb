8ffacce978e06f5310418ae1f96f4aba6c498728
Maluma Tolley maluma_0014@yahoo.com <maluma_0014@yahoo.com>
2025-06-19 10:15:23 +0100

    fix-chat

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -7,24 +7,31 @@
 
 class QuestionsController < ApplicationController
   before_action :set_renegotiation, only: [:create] # LS added
-  # [...]
   def create
-    @question = @renegotiation.questions.build(question_params)
-    @question.user = current_user
+  @question = @renegotiation.questions.build(question_params)
+  @question.user = current_user
 
   if @question.save
     respond_to do |format|
       format.turbo_stream {
-        render turbo_stream:
+        render turbo_stream: [
+          # 1) append the new message bubble
           turbo_stream.append(
             "questions",
-          partial: "renegotiations/question",
-          locals: { question: @question }
-        )
+            partial: "renegotiations/question",
+            locals: { question: @question }
+          ),
+          # 2) reset the form by replacing it with a fresh empty one
+          turbo_stream.replace(
+            "new_question",
+            partial: "renegotiations/form",
+            locals: { question: Question.new, renegotiation: @renegotiation }
+          )
+        ]
       }
       format.html { redirect_to renegotiation_path(@renegotiation) }
     end
-    else
+  else
       # On failure, reload whatever Renegotiations#show needs…
       @product   = @renegotiation.product
       @supplier  = @renegotiation.supplier
