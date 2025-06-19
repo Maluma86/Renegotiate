16ee05eb4dd12811c9eb3f6a254c24e06bedc140
Maluma Tolley maluma_0014@yahoo.com <maluma_0014@yahoo.com>
2025-06-19 11:54:43 +0100

    Testing

---
+++ db/migrate/20250619103804_install_solid_cable.rb
@@ -0,0 +1,9 @@
+class InstallSolidCable < ActiveRecord::Migration[7.1]
+  create_table "solid_cable_messages", force: :cascade do |t|
+    t.text "channel"
+    t.text "payload"
+    t.datetime "created_at", null: false
+    t.index ["channel"], name: "index_solid_cable_messages_on_channel"
+    t.index ["created_at"], name: "index_solid_cable_messages_on_created_at"
+  end
+end
